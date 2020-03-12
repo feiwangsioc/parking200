3661772ac57f9acc0f7727d77f73ddac89385084
Fei Wang <feiwangsioc@gmail.com>
2020-03-11 23:01:47 -0400

    devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,6 +58,9 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
